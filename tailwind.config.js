77f5360befa09df6f606a498629d539a61b8a605
Арсен Полівка <Arsen.Polivka@lnu.edu.ua>
2024-01-28 23:47:07 +0200

    [6]: button component created, styles structurized, main font added

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#F1D204'
+      },
+      fontFamily: {
+        'main': ['UpheavalPro', 'sans-serif']
+      },
+    },
   },
   plugins: [],
 }
