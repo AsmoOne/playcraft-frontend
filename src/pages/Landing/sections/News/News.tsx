cfb912dd92f60094c4fc63e0d4e40e89a6c73873
PolivkaArsen <arsen.polivka@tetalab.com>
2024-02-06 11:21:09 +0200

    [11]: news section created

--- src/pages/Landing/sections/News/News.tsx
+++ src/pages/Landing/sections/News/News.tsx
@@ -1,12 +1,18 @@
-import { Container } from "../../../../components/Layout/Container"
-import { NewsCard } from "../../../../components/NewsCard"
+import { Container } from "../../../../components/Layout/Container";
+import { NewsCard } from "../../../../components/NewsCard";
 
 export const News = () => {
   return (
-    <Container rootClassNames="flex flex-col gap-10">
-      <NewsCard />
-      <NewsCard />
-      <NewsCard />
+    <Container rootClassNames="-mt-15 md:pt-20">
+      <h2 className="text-h2-mobile font-main mb-7 md:mb-10 md:text-h2-desktop">
+        Останні новини
+      </h2>
+
+      <div className="flex flex-col gap-10">
+        <NewsCard />
+        <NewsCard />
+        <NewsCard />
+      </div>
     </Container>
   )
 }
